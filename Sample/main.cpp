dd3c564699c9e4a23917721fb5269f21367af439
andyyzhengg <azheng5@binghamton.edu>
2015-07-22 23:17:45 -0400

    Moving ball

--- Sample/main.cpp
+++ Sample/main.cpp
@@ -1,9 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Keyboard.hpp>
 
 int main() 
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
+  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+  sf::CircleShape shape(10.f);
   shape.setFillColor(sf::Color::Green);
   
   while(window.isOpen())
@@ -13,6 +14,22 @@ int main()
 	{
 	  if (event.type == sf::Event::Closed)
 	    window.close();
+	  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	    {
+	      shape.move(-1.0,0);
+	    }
+	  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	    {
+	      shape.move(1.0,0);
+	    }
+	  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	    {
+	      shape.move(0,-1.0);
+	    }
+	  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	    {
+	      shape.move(0,1.0);
+	    }
 	}
       window.clear();
       window.draw(shape);
